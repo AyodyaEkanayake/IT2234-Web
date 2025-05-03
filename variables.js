53034084572976eaaeb78358c2f3dd000fd1e585
AyodyaEkanayake <ayodyae822@gmail.com>
2025-05-04 00:06:12 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,15 @@
+//variables in js
+
+let num1 = 10
+let num2= 20
+
+console.log(num1 + num2)
+
+
+var x=30
+var x= 50   // var didn't pop up error if it declared the same varible again 
+var y=60
+
+console.log(x+y)
+
+const z=30  //const values cannot be reassigned 
