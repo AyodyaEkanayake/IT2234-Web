5784fd1db61bbc39f6499875ea3381a7232fc8c1
AyodyaEkanayake <ayodyae822@gmail.com>
2025-05-04 00:03:58 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,21 @@
+//print numbers from 1 to 10
+for(var i=1; i <=10; i++)
+{
+   console.log(i)
+}
+
+console.log();
+
+//print only the odd numbers 
+for(var i=1; i <= 10; i= i+2)
+{
+	console.log(i)
+}
+
+console.log();
+
+//print the numbers in reverse 
+for(var i=10; i >= 1; i--)
+{
+	console.log(i);
+}
