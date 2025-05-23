f3d0d3adbbd7e188aed62b2a8f0fd543fe0720c5
AyodyaEkanayake <ayodyae822@gmail.com>
2025-05-24 02:11:52 +0530

    Create printFunctions.js

---
+++ printFunctions.js
@@ -0,0 +1,25 @@
+console.log("Print functions in JS")
+function printMsg()
+{
+	console.log("Hello JS")
+}
+printMsg()
+console.log("--------------------")
+
+function sum()
+{
+	return 4+6
+}
+console.log(sum())
+console.log("--------------------")
+
+function sub(a,b)
+{
+	return a-b
+}
+
+console.log(sub(5,3))
+
+
+
+
