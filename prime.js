c8c1df9728d98e19bec70bf0446303e6beb6b32c
AyodyaEkanayake <ayodyae822@gmail.com>
2025-05-24 02:11:07 +0530

    Create prime.js

---
+++ prime.js
@@ -0,0 +1,20 @@
+//write a boolean function to find a given number is prime
+console.log("Prime")
+function prime(num)
+{
+	if(num < 2)return false;
+	
+	for(let i=2; i<= Math.sqrt(num); i++)
+	{
+		if(num%i == 0)return false;
+	}
+	return true;
+}
+
+console.log(prime(5)); 
+console.log(prime(10));
+
+
+
+
+
